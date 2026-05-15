0a4f4931b24d68aa231018514afdddabe9153825
codewithpandu <pandusetiadarmawan01@gmail.com>
2026-05-15 19:35:21 +0700

    fix: url

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -15,7 +15,7 @@
                     <div class="flex justify-between mt-auto">
                         <a href="/project/{{ $project->slug }}" class="text-brand inline-flex items-center mt-3">Read More
                         </a>
-                        <a href="{{ $project->url }}" class="text-brand inline-flex items-center mt-3">Demo
+                        <a target="_blank" href="{{ $project->url }}" class="text-brand inline-flex items-center mt-3">Demo
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                 stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
